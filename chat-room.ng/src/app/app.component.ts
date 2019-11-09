ddd7e9818f346b4ffb2a8e80789244aeaaef92de
Javier Villarreal <javiervillarreal@kpmg.com>
2019-11-09 17:30:15 -0600

    Chat client server working

--- chat-room.ng/src/app/app.component.ts
+++ chat-room.ng/src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
-import { HubConnection } from '@aspnet/signalr';
+import { Component, OnInit } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from '@aspnet/signalr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'chat-room';
 
+  name = '';
+  message = '';
+  messages: string[] = [];
+
   hubConnection: HubConnection;
+
+  ngOnInit() {
+    this.name = window.prompt('Name:', 'Javier');
+
+    this.hubConnection = new HubConnectionBuilder().withUrl('http://localhost:5200/chat-room').build();
+
+    this.hubConnection
+      .start()
+      .then(() => console.log('Connection started!'))
+      .catch(err => console.log('Error while establishing connection :('));
+
+    this.hubConnection.on('sendToAll', (name: string, message: string) => {
+      const text = `${name}: ${message}`;
+      this.messages.push(text);
+    });
+  }
+
+  sendMessage() {
+    this.hubConnection.send('sendToAll', this.name, this.message).catch(err => console.error(err));
+  }
 }
--- chat-room.web/Startup.cs
+++ chat-room.web/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,17 +17,21 @@ namespace ChatRoom.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Adding CORS to support spa access from angular development port
+            services.AddCors((options) =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                {
+                    builder
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials()
+                        .WithOrigins("http://localhost:4200"); // ng default dev port
+                });
+            });
+            
             // Adding signalR services
             services.AddSignalR();
-            
-            // Adding CORS to support spa access from angular development port
-            services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
-            {
-                builder
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .WithOrigins("http://localhost:4200"); // ng default dev port
-            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
