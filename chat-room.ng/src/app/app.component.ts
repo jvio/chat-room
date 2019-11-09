6ff27c927caefe3f3d393f583185ee7c5ca7cdc2
Javier Villarreal <javiervillarreal@kpmg.com>
2019-11-09 15:22:37 -0600

    Adding prettier for formatting and signalR client

--- chat-room.ng/src/app/app.component.ts
+++ chat-room.ng/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HubConnection } from '@aspnet/signalr';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chat-room';
+
+  hubConnection: HubConnection;
 }
