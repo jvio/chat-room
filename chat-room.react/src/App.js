b8d31fed5f2a45aa362983ff871f43d094b59ca8
Javier Villarreal <VillarrJ@mskcc.org>
2021-10-01 18:19:43 -0400

    Adding the behavior and interaction using react-bootstrap.

--- chat-room.react/src/App.js
+++ chat-room.react/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.scss';
+import {Dropdown} from "react-bootstrap";
 
 function App() {
   return (
@@ -22,10 +23,39 @@ function App() {
         <button type="button" className="btn btn-primary position-relative">
           Inbox
           <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-    99+
-    <span className="visually-hidden">unread messages</span>
-  </span>
+            99+
+            <span className="visually-hidden">unread messages</span>
+          </span>
         </button>
+
+        <br />
+
+        <div className="dropdown">
+          <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
+                  data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown button
+          </button>
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </div>
+
+        <br />
+
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Dropdown Button
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
       </header>
     </div>
   );
