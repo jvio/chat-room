29c969c1a279fb36f9c43b52a847270e7a11daec
Javier Villarreal <VillarrJ@mskcc.org>
2021-10-01 18:09:21 -0400

    Adding bootstrap to the project, all sass and theme overrides by adjusting the variables.

--- chat-room.react/src/App.js
+++ chat-room.react/src/App.js
@@ -17,6 +17,15 @@ function App() {
         >
           Learn React !!
         </a>
+
+
+        <button type="button" className="btn btn-primary position-relative">
+          Inbox
+          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    99+
+    <span className="visually-hidden">unread messages</span>
+  </span>
+        </button>
       </header>
     </div>
   );
